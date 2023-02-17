3e8db838389739754cfcd705e738bc390c03c73e
marikadu <marika.duhhanina@student.hamk.fi>
2023-02-17 07:57:24 +0200

    removed animation for the header

--- header.php
+++ header.php
@@ -8,7 +8,6 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="styles.css">
-
 </head>
 <body>
 
