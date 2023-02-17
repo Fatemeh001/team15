dd98403e80740caeaf8b6e8945df81270a7e2c85
armelant <56300747+armelant@users.noreply.github.com>
2023-02-17 09:55:57 +0200

    cart

--- header.php
+++ header.php
@@ -14,6 +14,23 @@
     color: black;
 }
     </style>
+
+
+<meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!-- Title  -->
+    <title>Essence - Fashion Ecommerce Template</title>
+
+    <!-- Favicon  -->
+    <link rel="icon" href="img/core-img/favicon.ico">
+
+    <!-- Core Style CSS -->
+    <link rel="stylesheet" href="css/core-style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <div class="container" >
@@ -23,21 +40,136 @@
    
 
 
-    <header>
-        <div class="container">
-          <div class="logo">
-            <a href="index.php"><img src="images/logo.png" width="400" heigth="auto" alt="Cecile"></a>
-          </div>
-  
-        
-            
-          <ul class="horizontal-list">
-            <li><a id="Tops" href="tops.php">Tops</a></li>
-            <li><a id="Sale" href="sale.php">Sale</a></li>>
-            <li><a id="Tops" href="signup.php">Sign up</a></li>
-            <li><a id="Sale" href="login.php">Login</a></li>>
-          </ul>
+  <header class="header_area">
+        <div class="classy-nav-container breakpoint-off d-flex align-items-center justify-content-between">
+            <!-- Classy Menu -->
+            <nav class="classy-navbar" id="essenceNav">
+                <!-- Logo -->
+                <a class="nav-brand" href="index.html"><img src="img/" alt=""></a>
+                <!-- Navbar Toggler -->
+                <div class="classy-navbar-toggler">
+                    <span class="navbarToggler"><span></span><span></span><span></span></span>
+                </div>
+                <!-- Menu -->
+                <div class="classy-menu">
+                    <!-- close btn -->
+                    <div class="classycloseIcon">
+                        <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                    </div>
+                    <!-- Nav Start -->
+                    <div class="classynav">
+                      <ul class="navbar-nav ms-auto mb-2 mb-lg-0 links">
+                        <li class="nav-item">
+                          <a class="nav-link active" aria-current="page" href="tops.php">Tops</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link active" aria-current="page" href="sale.php">Sale</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link active" aria-current="page" href="signup.php">Sign Up</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link active" aria-current="page" href="login.php">Login</a>
+                        </li>        
+                      </ul>
+                    </div>
+                    <!-- Nav End -->
+                </div>
+            </nav>
+
+            <!-- Header Meta Data -->
+            <div class="header-meta d-flex clearfix justify-content-end">
+               
+                <!-- Cart Area -->
+                <div class="cart-area">
+                    <a href="#" id="essenceCartBtn"><img src="images/Icons/Shoppingcart.png" alt=""> </a>
+                </div>
+            </div>
+
+        </div>
+    </header>
+    <!-- ##### Header Area End ##### -->
+
+    <!-- ##### Right Side Cart Area ##### -->
+    <div class="cart-bg-overlay"></div>
+
+    <div class="right-side-cart-area">
+
+        <!-- Cart Button -->
+        <div class="cart-button">
+            <a href="#" id="rightSideCart"><img src="images/Icons/Shoppingcart.png" alt=""> <span></span></a>
         </div>
 
+        <div class="cart-content d-flex">
 
-        <div class="line-divider"></div>
+            <!-- Cart List Area -->
+            <div class="cart-list">
+                <!-- Single Cart Item -->
+                <div class="single-cart-item">
+                    <a href="#" class="product-image">
+                        <img src="images/model7.png" class="cart-thumb" alt="">
+                        <!-- Cart Item Desc -->
+                        <div class="cart-item-desc">
+                          <span class="product-remove"><i class="fa fa-close" aria-hidden="true"></i></span>
+                            <span class="badge"></span>
+                            <h6></h6>
+                            <p class="size">Size: M</p>
+                            <p class="color">Color: Black</p>
+                            <p class="price">Price: 55.00€</p>
+                        </div>
+                    </a>
+                </div>
+
+                <!-- Single Cart Item -->
+                <div class="single-cart-item">
+                    <a href="#" class="product-image">
+                        <img src="images/model6.png" class="cart-thumb" alt="">
+                        <!-- Cart Item Desc -->
+                        <div class="cart-item-desc">
+                          <span class="product-remove"><i class="fa fa-close" aria-hidden="true"></i></span>
+                          <span class="badge"></span>
+                            <h6></h6>
+                            <p class="size">Size: M</p>
+                            <p class="color">Color: Black</p>
+                            <p class="price">Price: 55.00€</p>
+                        </div>
+                    </a>
+                </div>
+
+                <!-- Single Cart Item -->
+                <div class="single-cart-item">
+                    <a href="#" class="product-image">
+                        <img src="images/model5.png" class="cart-thumb" alt="">
+                        <!-- Cart Item Desc -->
+                        <div class="cart-item-desc">
+                          <span class="product-remove"><i class="fa fa-close" aria-hidden="true"></i></span>
+                          <span class="badge"></span>
+                            <h6></h6>
+                            <p class="size">Size: M</p>
+                            <p class="color">Color: Black</p>
+                            <p class="price">Price: 55.00€</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <!-- Cart Summary -->
+            <div class="cart-amount-summary">
+
+                <h2>Summary</h2>
+                <ul class="summary-table">
+                    <li><span>subtotal:</span> <span>0.00€</span></li>
+                    <li><span>delivery:</span> <span>Free</span></li>
+                    <li><span>discount:</span> <span>0%</span></li>
+                    <li><span>total:</span> <span>0.00€</span></li>
+                </ul>
+                <div class="checkout-btn mt-100">
+                    <a href="checkout.html" class="btn essence-btn">check out</a>
+                </div>
+            </div>
+        </div>
+    </div>
+       <!-- jQuery (Necessary for All JavaScript Plugins) -->
+       <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <!-- Active js -->
+    <script src="js/active.js"></script>
