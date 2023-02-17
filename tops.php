bd6c64c8177e5cf491f7d98346331a4df464da56
marikadu <marika.duhhanina@student.hamk.fi>
2023-02-17 07:34:02 +0200

    added universal header for tops.php and sale.php

--- tops.php
+++ tops.php
@@ -1,32 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Cecile | Home</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="tops.css">
-</head>
-<body>
-
-    <header>
-        <div class="container">
-          <div class="logo">
-            <a href="index.php"><img src="images/logo.png" width="400" heigth="auto" alt="Cecile"></a>
-          </div>
-  
-        
-  
-          <ul class="horizontal-list">
-            <li><a id="Tops" href="tops.php">Tops</a></li>
-            <li><a id="Sale" href="sale.php">Sale</a></li>
-          </ul>
-        </div>
-
-
-        <div class="line-divider"></div>
+<?php include "header.php" ?>
 
   <div class="container">
 
