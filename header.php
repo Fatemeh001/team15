39046a1f7310b9aab7f07fbf909551e00d1246bf
armelant <56300747+armelant@users.noreply.github.com>
2023-02-18 20:14:11 +0200

    cart updates

--- header.php
+++ header.php
@@ -1,103 +1,64 @@
-
-    <!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-  <title>Cecile | Home</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="styles.css">
-  <style>
-  a, .nav-link,.navbar-brand {
-    color: black;
-}
-    </style>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/header.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link rel="icon" href="images/1.png" type="image/x-icon">
 
-
-<meta charset="UTF-8">
-    <meta name="description" content="">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    
-    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-
-    <!-- Title  -->
-    <title>Essence - Fashion Ecommerce Template</title>
-
-    <!-- Favicon  -->
-    <link rel="icon" href="img/core-img/favicon.ico">
-
-    <!-- Core Style CSS -->
-    <link rel="stylesheet" href="css/core-style.css">
-    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-<div class="container" >
-    <div class="row">
-  <nav class="navbar navbar-expand-lg" style="background-color: white" >
-  <div class="container-fluid">
-   
-
-
-  <header class="header_area">
-        <div class="classy-nav-container breakpoint-off d-flex align-items-center justify-content-between">
-            <!-- Classy Menu -->
-            <nav class="classy-navbar" id="essenceNav">
-                <!-- Logo -->
-                <a class="nav-brand" href="index.html"><img src="img/" alt=""></a>
-                <!-- Navbar Toggler -->
-                <div class="classy-navbar-toggler">
-                    <span class="navbarToggler"><span></span><span></span><span></span></span>
-                </div>
-                <!-- Menu -->
-                <div class="classy-menu">
-                    <!-- close btn -->
-                    <div class="classycloseIcon">
-                        <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
-                    </div>
-                    <!-- Nav Start -->
-                    <div class="classynav">
-                      <ul class="navbar-nav ms-auto mb-2 mb-lg-0 links">
-                        <li class="nav-item">
-                          <a class="nav-link active" aria-current="page" href="tops.php">Tops</a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link active" aria-current="page" href="sale.php">Sale</a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link active" aria-current="page" href="signup.php">Sign Up</a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link active" aria-current="page" href="login.php">Login</a>
-                        </li>        
-                      </ul>
-                    </div>
-                    <!-- Nav End -->
-                </div>
-            </nav>
-
-            <!-- Header Meta Data -->
-            <div class="header-meta d-flex clearfix justify-content-end">
-               
-                <!-- Cart Area -->
-                <div class="cart-area">
-                    <a href="#" id="essenceCartBtn"><img src="images/Icons/Shoppingcart.png" alt=""> </a>
-                </div>
-            </div>
-
+  <header>
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid my-class-with-padding">
+        <div class="cotainer-fluid img-1">
+        <a class="nav-brand" href="index.php"><img src="images/LogoBig.png" alt=""></a>
         </div>
-    </header>
-    <!-- ##### Header Area End ##### -->
+        <div class="classy-navbar-toggler">
+                    <span class="navbarToggler"><span></span><span></span><span></span></span>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0 links">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="tops.php">Tops</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="sale.php">Sale</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="signup.php">Sign Up</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="login.php">Login</a>
+            </li>
+          </ul>
+          <ul class="navbar-nav mb-2 mb-lg-0 cart">
+            <li class="nav-item">
+              <div class="header-meta d-flex clearfix justify-content-end">
+                <div class="cart-area">
+                    <a href="#" id="essenceCartBtn"><img src="images/Icons/Shoppingcart.png" alt="cart"> </a>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
 
-    <!-- ##### Right Side Cart Area ##### -->
-    <div class="cart-bg-overlay"></div>
+  <div class="cart-bg-overlay"></div>
 
     <div class="right-side-cart-area">
 
         <!-- Cart Button -->
         <div class="cart-button">
-            <a href="#" id="rightSideCart"><img src="images/Icons/Shoppingcart.png" alt=""> <span></span></a>
+            <a href="#" id="rightSideCart"><img src="images/Icons/Shoppingcart.png" alt=""></a>
         </div>
 
         <div class="cart-content d-flex">
@@ -112,10 +73,10 @@
                         <div class="cart-item-desc">
                           <span class="product-remove"><i class="fa fa-close" aria-hidden="true"></i></span>
                             <span class="badge"></span>
-                            <h6></h6>
-                            <p class="size">Size: M</p>
-                            <p class="color">Color: Black</p>
-                            <p class="price">Price: 55.00€</p>
+                            <h6>Sweater</h6>
+                            <p class="size">Size: S</p>
+                            <p class="color">Color: White</p>
+                            <p class="price">70.00€</p>
                         </div>
                     </a>
                 </div>
@@ -123,15 +84,15 @@
                 <!-- Single Cart Item -->
                 <div class="single-cart-item">
                     <a href="#" class="product-image">
-                        <img src="images/model6.png" class="cart-thumb" alt="">
+                        <img src="images/model7.png" class="cart-thumb" alt="">
                         <!-- Cart Item Desc -->
                         <div class="cart-item-desc">
                           <span class="product-remove"><i class="fa fa-close" aria-hidden="true"></i></span>
                           <span class="badge"></span>
-                            <h6></h6>
-                            <p class="size">Size: M</p>
-                            <p class="color">Color: Black</p>
-                            <p class="price">Price: 55.00€</p>
+                            <h6>Sweater</h6>
+                            <p class="size">Size: S</p>
+                            <p class="color">Color: White</p>
+                            <p class="price">70.00€</p>
                         </div>
                     </a>
                 </div>
@@ -139,15 +100,15 @@
                 <!-- Single Cart Item -->
                 <div class="single-cart-item">
                     <a href="#" class="product-image">
-                        <img src="images/model5.png" class="cart-thumb" alt="">
+                        <img src="images/model7.png" class="cart-thumb" alt="">
                         <!-- Cart Item Desc -->
                         <div class="cart-item-desc">
                           <span class="product-remove"><i class="fa fa-close" aria-hidden="true"></i></span>
                           <span class="badge"></span>
-                            <h6></h6>
-                            <p class="size">Size: M</p>
-                            <p class="color">Color: Black</p>
-                            <p class="price">Price: 55.00€</p>
+                            <h6>Sweater</h6>
+                            <p class="size">Size: S</p>
+                            <p class="color">Color: White</p>
+                            <p class="price">70.00€</p>
                         </div>
                     </a>
                 </div>
@@ -160,7 +121,7 @@
                 <ul class="summary-table">
                     <li><span>subtotal:</span> <span>0.00€</span></li>
                     <li><span>delivery:</span> <span>Free</span></li>
-                    <li><span>discount:</span> <span>0%</span></li>
+                    <li><span>discount:</span> <span>%</span></li>
                     <li><span>total:</span> <span>0.00€</span></li>
                 </ul>
                 <div class="checkout-btn mt-100">
@@ -169,7 +130,9 @@
             </div>
         </div>
     </div>
-       <!-- jQuery (Necessary for All JavaScript Plugins) -->
-       <script src="js/jquery/jquery-2.2.4.min.js"></script>
-    <!-- Active js -->
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
     <script src="js/active.js"></script>
+
+  <div class="container">
+    <div class="row breadcrumbs">
+      <div class="col-12 p-4">
