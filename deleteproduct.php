1d245d2760872e932509bf305c6d2ca9046d5e21
Fatemeh001 <fatemeh22001@student.hamk.fi>
2023-02-18 22:19:13 +0200

    end

--- deleteproduct.php
+++ deleteproduct.php
@@ -4,5 +4,24 @@ include 'db.php';
 include "header.php";
 
 
+if (isset($_POST['delete_product'])) {
+    $product_id = $_POST['product_id'];
+
+    $sql = "DELETE FROM product WHERE product_id=$product_id";
+    if (mysqli_query($conn, $sql)) {
+        echo "Product deleted successfully.";
+    } else {
+        echo "Error deleting product: " . mysqli_error($conn);
+    }
+}
+
+mysqli_close($conn);
+?>
+
+<form action="" method="post">
+    <input type="text" name="product_id" placeholder="Product ID">
+    <input type="submit" name="delete_product" value="Delete Product">
+</form>
+
 
 
