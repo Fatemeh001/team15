ecb8ab3ff6fc8c9e1eb1f04d634649b718845f75
Fatemeh001 <fatemeh22001@student.hamk.fi>
2023-02-18 21:47:11 +0200

    a

--- updateproduct.php
+++ updateproduct.php
@@ -34,7 +34,7 @@ if (isset($_POST['delete_product'])) {
     }
 }
 
-$sql = "SELECT * FROM product WHERE deleted_at IS NULL";
+$sql = "SELECT * FROM product WHERE deleted_at IS NOT NULL";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
@@ -58,6 +58,47 @@ include "footetr.php";
     <input type="text" name="price" placeholder="Price"><br><br>
     <input type="text" name="quantity" placeholder="
       
+
+
+
+
+    <?php
+$title = "Update your info";
+include "header.php";
+$a = $_GET['product_id']; 
+include 'db.php'; 
+$result = mysqli_query($con,"select * from studentinfo 
+where product_id = '$a' "); 
+$row = mysqli_fetch_array($result); 
+?> 
+<h2>Update your information below</h2> 
+<form name ="update"  method = "post" action = ""> 
+<input type="text" name="name" placeholder="Name" required value="<?php echo $row['name']; ?>"><br><br>
+    <input type="text" name="description" placeholder="Description" required value="<?php echo $row['description']; ?>"><br><br> 
+    <input type="text" name="inventory_id" placeholder="Inventory ID" required value="<?php echo $row['inventory_id']; ?>"><br><br> 
+    <input type="text" name="price" placeholder="Price" required value="<?php echo $row['price']; ?>"><br><br> 
+    <input type="text" name="quantity" placeholder="quantity" required value="<?php echo $row['quantity']; ?>"><br><br> 
+ 
+</select> 
+<br><br> 
+ 
+<input type="submit" value="Update" name ="update"> 
+<br><br> 
+<input type="submit" value="Delete" name ="delete"> 
+ 
+</form> 
+ 
+<?php 
+if (isset($_POST['update'])){ 
+    $fname = $_POST['fname']; 
+    $lname = $_POST['lname']; 
+    $query = mysqli_query($conn,"UPDATE studentinfo set fname='$fname', lname='$lname' where id='$a' "); 
+    if ($query){ 
+        echo "<h2>Your information is updated successfully</h2> "; 
+    } 
+    else { echo "<h2>Your information is not updated successfully</h2> ";} 
+} 
+?>
 
      
 
