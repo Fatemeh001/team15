6dc518fc35e8064c8e30adb2c9534b19a960dcd1
Fatemeh001 <fatemeh22001@student.hamk.fi>
2023-02-18 22:13:37 +0200

    end form update

--- updateproduct.php
+++ updateproduct.php
@@ -1,47 +1,32 @@
 <?php
-$title = "Update your info";
-include "header.php";
-$a = $_GET['product_id']; 
-include 'db.php'; 
-$result = mysqli_query($con,"select * from studentinfo 
-where product_id = '$a' "); 
-$row = mysqli_fetch_array($result); 
-?> 
-<h2>Update your information below</h2> 
-<form name ="update"  method = "post" action = ""> 
-<input type="text" name="name" placeholder="Name" required value="<?php echo $row['name']; ?>"><br><br>
-    <input type="text" name="description" placeholder="Description" required value="<?php echo $row['description']; ?>"><br><br> 
-    <input type="text" name="inventory_id" placeholder="Inventory ID" required value="<?php echo $row['inventory_id']; ?>"><br><br> 
-    <input type="text" name="price" placeholder="Price" required value="<?php echo $row['price']; ?>"><br><br> 
-    <input type="text" name="quantity" placeholder="quantity" required value="<?php echo $row['quantity']; ?>"><br><br> 
- 
-</select> 
-<br><br> 
- 
-<input type="submit" value="Update" name ="update"> 
-<br><br> 
-<input type="submit" value="Delete" name ="delete"> 
- 
-</form> 
- 
-<?php 
-if (isset($_POST['update'])){
+include 'db.php';
+
+if (isset($_POST['update_product'])) {
+    $product_id = $_POST['product_id'];
     $name = $_POST['name'];
     $description = $_POST['description'];
     $inventory_id = $_POST['inventory_id'];
     $price = $_POST['price'];
-    $deleted_at = date("Y-m-d H:i:s");
+    $modified_at = date("Y-m-d H:i:s");
     $quantity = $_POST['quantity'];
 
-    $query = mysqli_query($conn,"UPDATE studentinfo set name='$name', description='$description' , inventory_id='$inventory_id',price='$price',
-    deleted_at='$deleted_at' ,quantity='$quantity' where product_id='$a' "); 
-    if ($query){ 
-        echo "<h2>Your information is updated successfully</h2> "; 
-    } 
-    else { echo "<h2>Your information is not updated successfully</h2> ";} 
-} 
+    $sql = "UPDATE product SET name='$name', description='$description', inventory_id=$inventory_id, price=$price, modified_at='$modified_at', quantity=$quantity WHERE product_id=$product_id";
+    if (mysqli_query($conn, $sql)) {
+        echo "Product updated successfully.";
+    } else {
+        echo "Error updating product: " . mysqli_error($conn);
+    }
+}
+
+mysqli_close($conn);
 ?>
 
-     
-
-
+<form action="" method="post">
+    <input type="text" name="product_id" placeholder="Product ID">
+    <input type="text" name="name" placeholder="Name">
+    <input type="text" name="description" placeholder="Description"><br><br>
+    <input type="text" name="inventory_id" placeholder="Inventory ID">
+    <input type="text" name="price" placeholder="Price"><br><br>
+    <input type="text" name="quantity" placeholder="Quantity">
+    <input type="submit" name="update_product" value="Update Product">
+</form>
