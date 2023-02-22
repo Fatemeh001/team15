c97f1be6c8c6e84dcf715fab47c9784004baff12
Fatemeh001 <fatemeh22001@student.hamk.fi>
2023-02-22 22:39:40 +0200

    update

--- favoritlist.php
+++ favoritlist.php
@@ -2,3 +2,16 @@
 $title = "Cecile | favorit List Product"?>
 <?php
 include 'db.php';
+
+if (isset($_POST['add_favorit'])) {
+    $product_id = $_POST['product_id'];
+    $user_id = $_SESSION['user_id'];
+    $price = $_POST['price'];
+    $in_the_flist = 1;
+
+
+     $sql = "INSERT INTO favorite_list (product_id,user_id,  price,  in_the_flist) 
+     VALUES ($product_id,$user_id,  $price, $in_the_flist)";
+     
+
+}
