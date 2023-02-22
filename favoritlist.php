50f85d566bcd1f1c8f9486281695f03c8fd8183a
Fatemeh001 <fatemeh22001@student.hamk.fi>
2023-02-22 22:44:11 +0200

    a

--- favoritlist.php
+++ favoritlist.php
@@ -12,6 +12,26 @@ if (isset($_POST['add_favorit'])) {
 
      $sql = "INSERT INTO favorite_list (product_id,user_id,  price,  in_the_flist) 
      VALUES ($product_id,$user_id,  $price, $in_the_flist)";
-     
+     if (mysqli_query($conn, $sql)) {
+        echo "Product added to favorites successfully.";
+    } else {
+        echo "Error adding product to favorites: " . mysqli_error($conn);
+    }
+
 
 }
+
+
+mysqli_close($conn);
+?>
+<form action="" method="post">
+    <input type="hidden" name="product_id" value="<?php echo $product_id; ?>">
+    <input type="text" name="name" placeholder="Name">
+    <input type="text" name="description" placeholder="Description"><br><br>
+</form>
+
+
+
+
+<?php include "footer.php";
+?>
