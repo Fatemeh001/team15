dd3390ff35561516fed3f86bcdcd113010c761f4
marikadu <marika.duhhanina@student.hamk.fi>
2023-02-22 15:09:19 +0200

    removed unwanted character in footer

--- feedbackThanks.php
+++ feedbackThanks.php
@@ -1,6 +1,12 @@
 <?php include "header.php";
 $title = "Cecile | Thanks for the feedback!";?>
-
+<style>
+    .thank-you-for-the-feedback{
+    margin: 0 auto;
+    text-align: center;
+    padding-bottom: 20px;
+    }
+    </style>
 
 <section class="thank-you-for-the-feedback">
     
