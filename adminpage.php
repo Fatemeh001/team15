b1311d0668617665879fed0694638555200017b5
marikadu <marika.duhhanina@student.hamk.fi>
2023-03-06 10:41:29 +0200

    changed feedback page and changed admin page

--- adminpage.php
+++ adminpage.php
@@ -1,10 +1,20 @@
-<?php
+<?php $title = "Cecile | Admin Page";
+include "header.php"; ?>
+
+
+<style>
+    .middle{
+        margin: 0 auto;
+        text-align: center;
+        font-size: 30px;
+        padding-bottom: 30px;
+    }
+</style>
+
+<div class="middle">
+<li><a id="Tops" href="feedbackRead.php">See received Feedback</a></li>
+</div>
 
 
 
-echo "Yaasdsdy";
-
-
-
-
-?>
+<?php include "footer.php"; ?>
---
+++ feedbackRead.php
@@ -0,0 +1,43 @@
+<?php $title = "Cecile | Read Feedback";
+include "header.php"; 
+include 'db.php';?>
+
+<style>
+    .table{
+    margin: 0 auto;
+    margin-left: 100px;
+    margin-right: 100px;
+    text-align: center;
+    }
+    </style>
+
+<?php
+$sql = "select * from feedbackNew";
+$result = $conn -> query($sql);
+
+if ($result -> num_rows > 0) {
+    echo "<table class = 'table'>
+        <tr>
+        <th>E-mail</th>
+        <th>Rating</th>
+        <th>Feedback Text</th>
+        </tr>";
+
+    while($row = $result -> fetch_assoc()){
+    echo "
+        <tr>
+        <th>$row[email]</th>
+        <th>$row[rating]</th>
+        <th>$row[feedbackText]</th>
+        </tr>";
+    }
+    echo "</table>";
+}
+
+else {
+    echo "No results";
+}
+$conn -> close();
+?>
+
+<?php include "footer.php" ?>
