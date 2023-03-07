f3576247a1566acb1fa1a624e573cb298d42ddcc
Fatemeh001 <fatemeh22001@student.hamk.fi>
2023-03-07 13:13:48 +0200

    a

--- index.php
+++ index.php
@@ -29,3 +29,10 @@ if (isset($_POST['unliked'])) {
 ?>
 <link rel="stylesheet" href="styles.css">
 <div class="container">
+<div class="p-5 text-center bg-image rounded-3" style="background-image: url('images/banner2.jpg');height: 600px;  background-repeat: no-repeat;">
+        <div class="mask" style="background-color: #ffe8e0d5">
+          <div class="d-flex justify-content-center align-items-center h-100">
+            <div class="text-white">
+               <h1 class="mb-3 " style="padding-top: 16px; padding-bottom: 16px;">Wear clothes that make you be yourself</h1>
+
+            </div>
