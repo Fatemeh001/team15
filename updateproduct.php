59df1fc17c803f8f8e3256a67cc07dbacd9f79bf
Fatemeh001 <fatemeh22001@student.hamk.fi>
2023-03-08 22:54:13 +0200

    k

--- updateproduct.php
+++ updateproduct.php
@@ -5,7 +5,7 @@ include "header.php";
 include 'db.php';
 
 if (isset($_POST['update_product'])) {
-    $product_id = $_POST['product_id'];
+    $id = $_POST['id'];
     $name = $_POST['name'];
     $description = $_POST['description'];
     $inventory_id = $_POST['inventory_id'];
@@ -13,7 +13,7 @@ if (isset($_POST['update_product'])) {
     $modified_at = date("Y-m-d H:i:s");
     $quantity = $_POST['quantity'];
 
-    $sql = "UPDATE product SET name='$name', description='$description', inventory_id=$inventory_id, price=$price, modified_at='$modified_at', quantity=$quantity WHERE product_id=$product_id";
+    $sql = "UPDATE fatemeh_product SET name='$name', description='$description', inventory_id=$inventory_id, price=$price, modified_at='$modified_at', quantity=$quantity WHERE id=$id";
     if (mysqli_query($conn, $sql)) {
         echo "Product updated successfully.";
     } else {
@@ -25,7 +25,7 @@ mysqli_close($conn);
 ?>
 
 <form action="" method="post" style="margin-left:35px ;">
-    <input type="text" name="product_id" placeholder="Product ID">
+    <input type="text" name="id" placeholder="Product ID">
     <input type="text" name="name" placeholder="Name">
     <input type="text" name="description" placeholder="Description"><br><br>
     <input type="text" name="inventory_id" placeholder="Inventory ID">
